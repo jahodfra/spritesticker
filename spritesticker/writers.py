98012b5e72934bb471096fa03ca125e82d2883c9
Franti?ek Jahoda <frantisek.jahoda@e-fractal.cz>
2010-01-14 12:03:10 +0100

    time added to image link to cause automatic cache reload for an image

--- spritesticker/writers.py
+++ spritesticker/writers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class CssWriter:
     def __init__(self):
         self.selectorToImage = {}
@@ -13,14 +15,15 @@ class CssWriter:
         pathPrefix - path prefix for spritesheets images in css file
         '''
         self.pathPrefix = pathPrefix
+        version = datetime.now().strftime('%Y-%m-%dT%H:%M')
 
         fout = file(filename, 'w')
         for selector, image in self.selectorToImage.items():
-            self._writeImageCss(fout, selector, image)
+            self._writeImageCss(fout, selector, image, version)
         fout.close()
 
-    def _writeImageCss(self, fout, selector, image):
-        imagePath = self.pathPrefix + image.filename
+    def _writeImageCss(self, fout, selector, image, version):
+        imagePath = self.pathPrefix + image.filename + '?' + version
         repeat = image.repeat
         color = image.color or ''
         pos = '%dpx %dpx' % image.pos
